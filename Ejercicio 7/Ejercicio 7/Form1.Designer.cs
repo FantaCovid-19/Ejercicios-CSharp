7a40be4cbb9c5a82176339c2c74ed2fa24710911
Mr.Fantasma <54487840+FantaCovid-19@users.noreply.github.com>
2022-05-11 09:02:11 -0400

    Add files via upload

---
+++ Ejercicio 7/Ejercicio 7/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Ejercicio_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.LoginMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.InicioMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoginMenu,
+            this.InicioMenu});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(800, 24);
+            this.Menu.TabIndex = 1;
+            this.Menu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Menu_ItemClicked);
+            // 
+            // LoginMenu
+            // 
+            this.LoginMenu.Name = "LoginMenu";
+            this.LoginMenu.Size = new System.Drawing.Size(49, 20);
+            this.LoginMenu.Text = "Login";
+            // 
+            // InicioMenu
+            // 
+            this.InicioMenu.Name = "InicioMenu";
+            this.InicioMenu.Size = new System.Drawing.Size(48, 20);
+            this.InicioMenu.Text = "Inicio";
+            this.InicioMenu.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Menu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.Menu;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        public System.Windows.Forms.ToolStripMenuItem InicioMenu;
+        public System.Windows.Forms.MenuStrip Menu;
+        public System.Windows.Forms.ToolStripMenuItem LoginMenu;
+    }
+}
+
