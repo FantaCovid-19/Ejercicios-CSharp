e4de007bbaab9b7b80bf09956d6214ff8d22a317
Mr.Fantasma <54487840+FantaCovid-19@users.noreply.github.com>
2022-05-25 11:45:20 -0400

    Add files via upload

---
+++ Ejercicio 8/Ejercicio 8/Form2.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Ejercicio_8
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.numTxtHours = new System.Windows.Forms.NumericUpDown();
+            this.cbGrade = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkOffDuty = new System.Windows.Forms.RadioButton();
+            this.checkOnDuty = new System.Windows.Forms.RadioButton();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numTxtId = new System.Windows.Forms.NumericUpDown();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvListJobs = new System.Windows.Forms.DataGridView();
+            this.txtRun = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtHours)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtId)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListJobs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cargo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Horas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(65, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(120, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(65, 93);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(120, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // numTxtHours
+            // 
+            this.numTxtHours.Location = new System.Drawing.Point(65, 124);
+            this.numTxtHours.Name = "numTxtHours";
+            this.numTxtHours.Size = new System.Drawing.Size(120, 20);
+            this.numTxtHours.TabIndex = 6;
+            // 
+            // cbGrade
+            // 
+            this.cbGrade.FormattingEnabled = true;
+            this.cbGrade.Location = new System.Drawing.Point(64, 153);
+            this.cbGrade.Name = "cbGrade";
+            this.cbGrade.Size = new System.Drawing.Size(121, 21);
+            this.cbGrade.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkOffDuty);
+            this.groupBox1.Controls.Add(this.checkOnDuty);
+            this.groupBox1.Location = new System.Drawing.Point(12, 189);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 50);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Estado:";
+            // 
+            // checkOffDuty
+            // 
+            this.checkOffDuty.AutoSize = true;
+            this.checkOffDuty.Location = new System.Drawing.Point(84, 19);
+            this.checkOffDuty.Name = "checkOffDuty";
+            this.checkOffDuty.Size = new System.Drawing.Size(83, 17);
+            this.checkOffDuty.TabIndex = 1;
+            this.checkOffDuty.TabStop = true;
+            this.checkOffDuty.Text = "Desabilitado";
+            this.checkOffDuty.UseVisualStyleBackColor = true;
+            // 
+            // checkOnDuty
+            // 
+            this.checkOnDuty.AutoSize = true;
+            this.checkOnDuty.Location = new System.Drawing.Point(6, 19);
+            this.checkOnDuty.Name = "checkOnDuty";
+            this.checkOnDuty.Size = new System.Drawing.Size(72, 17);
+            this.checkOnDuty.TabIndex = 0;
+            this.checkOnDuty.TabStop = true;
+            this.checkOnDuty.Text = "Habilitado";
+            this.checkOnDuty.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(64, 245);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ID:";
+            // 
+            // numTxtId
+            // 
+            this.numTxtId.Location = new System.Drawing.Point(64, 7);
+            this.numTxtId.Name = "numTxtId";
+            this.numTxtId.Size = new System.Drawing.Size(121, 20);
+            this.numTxtId.TabIndex = 11;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(215, 7);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(296, 7);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvListJobs
+            // 
+            this.dgvListJobs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListJobs.Location = new System.Drawing.Point(215, 36);
+            this.dgvListJobs.Name = "dgvListJobs";
+            this.dgvListJobs.Size = new System.Drawing.Size(559, 234);
+            this.dgvListJobs.TabIndex = 15;
+            // 
+            // txtRun
+            // 
+            this.txtRun.Location = new System.Drawing.Point(64, 36);
+            this.txtRun.Name = "txtRun";
+            this.txtRun.Size = new System.Drawing.Size(120, 20);
+            this.txtRun.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "RUN:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 276);
+            this.Controls.Add(this.txtRun);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgvListJobs);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.numTxtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbGrade);
+            this.Controls.Add(this.numTxtHours);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "CRUD Form";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtHours)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtId)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListJobs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.NumericUpDown numTxtHours;
+        private System.Windows.Forms.ComboBox cbGrade;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton checkOffDuty;
+        private System.Windows.Forms.RadioButton checkOnDuty;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numTxtId;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvListJobs;
+        private System.Windows.Forms.TextBox txtRun;
+        private System.Windows.Forms.Label label6;
+    }
+}
